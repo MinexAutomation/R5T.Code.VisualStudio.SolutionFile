67ca9e0f2f426ab28fc823bef9a793c5f1eecd90
MinexAutomation <minex.automation@gmail.com>
2019-06-25 13:22:35 -0400

    Initial commit of source-stub.

---
+++ source/R5T.Code.VisualStudio.SolutionFile.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Code.VisualStudio.SolutionFile.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Code.VisualStudio.SolutionFile/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Code.VisualStudio.SolutionFile
+{
+    public class Class1
+    {
+    }
+}
